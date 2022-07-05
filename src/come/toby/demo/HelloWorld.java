b6b18c2c19c0bc26ee9ebbb4f841359220a85f93
suta <sutuo0426@163.com>
2022-07-05 15:11:21 +0800

    Initial commit

---
+++ src/come/toby/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package come.toby.demo;
+
+/**
+ * Created by suta on 2022/7/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
